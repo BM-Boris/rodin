29076d4af43eaba7e4f07938440c3176a15cf5f6
BM-Boris <bb.minasenko@gmail.com>
2026-04-14 15:06:38 -0400

    ver

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rodin',
-    version='1.9.25',
+    version='1.9.26',
     packages=find_packages(),
     description='A comprehensive toolkit for processing and analyzing metabolomics data.',
     long_description=open('README.md').read(),
